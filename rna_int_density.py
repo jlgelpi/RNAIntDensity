bfbe00d64588f25be4e78f02a8d81cb93a1c7553
Josep Ll. Gelpi <gelpi@ub.edu>
2025-06-04 15:48:30 +0200

    Add suport for alt locations, hetatm residue names starting with number

--- rna_int_density.py
+++ rna_int_density.py
@@ -3,7 +3,7 @@
 
 import json
 import argparse
-import re
+
 import os
 import sys
 import numpy as np
@@ -14,111 +14,21 @@ from Bio.PDB.Chain import Chain
 from Bio.PDB.Model import Model
 from Bio.PDB.Residue import Residue
 from Bio.PDB.Atom import Atom
-from Bio.PDB.Superimposer import Superimposer
+
 from Bio.PDB.PDBIO import PDBIO
 
 import biobb_structure_checking.modelling.utils as mu
 
-RIBOSE_RING_ATOMS = ['C4\'', 'O4\'', 'C1\'', 'C2\'', 'C3\'']
+import utils as ut
 
-SUP_ATOMS = RIBOSE_RING_ATOMS + ['N1', 'N9', 'C1', 'C6']
 
-HB_QUALITY_ALL = ['standard', 'acceptable', 'questionable']
+SUP_ATOMS = ut.RIBOSE_RING_ATOMS + ut.BASE_ORIENT_ATOMS
 
 HB_QUALITY = ['standard', 'acceptable']
-
-NULL_ROTRAN = [
-    [[1.0, 0.0, 0.0],
-     [0.0,  1.0, 0.0],
-     [0.0, 0.0, 1.0]],
-    [0.0, 0.0, 0.0]
-]
-
-STD_RESNAMES = [
-    'A', 'C', 'G', 'U',
-    'DT', 'DA', 'DC', 'DG',
-    'ALA', 'ARG', 'ASN', 'ASP', 'CYS',
-    'GLY', 'GLN', 'GLU', 'HIS', 'ILE',
-    'LEU', 'LYS', 'MET', 'PHE', 'PRO',
-    'SER', 'THR', 'TRP', 'TYR', 'VAL'
-]
-
-STD_NUCLEOTIDES = [
-    'A', 'C', 'G', 'U',
-    'DA', 'DC', 'DG', 'DT'
-]
 
 NEW_CHAIN_ID = 'A'  # New chain ID for the output structure
 WAT_CHAIN_ID = 'W'  # Chain ID for water molecules in the output structure
 
-
-def get_atom_from_index(st, atom_index):
-    ''' Get atom from structure by index '''
-    for at in st.get_atoms():
-        if at.serial_number == atom_index:
-            return at
-    return None
-
-
-def get_atom_from_x3dna_id(st, atom_id):
-    ''' Get atom from structure by ID '''
-    at_id, res_id = atom_id.split('@')
-    chain_id, resid = res_id.split('.')
-    match = re.match(r"([A-Za-z]+)(\d+)", resid)
-    if match:
-        resname = match.group(1)  # 'GNG'
-        resnum = match.group(2)  # '101'
-        hetlb = ' '
-        if resname not in STD_RESNAMES:
-            hetlb = f"H_{resname}"
-        res = st[0][chain_id][(hetlb, int(resnum), ' ')]
-    else:
-        return None
-    return res.child_dict[at_id]
-
-
-def atom_renumbering(st):
-    """ Sets  Bio.PDB.Atom.serial_number for all atoms in the structure,
-        overriding original if any.
-    """
-    i = 1
-    for atm in st.get_atoms():
-        atm.serial_number = i
-        if hasattr(atm, 'selected_child'):
-            atm.selected_child.serial_number = i
-        i += 1
-
-
-def superimpose_models(st):
-    ''' superimpose groups of models in a structure '''
-    rmsd = [0.]
-    rmsd_all = [0.]
-    rotran = [NULL_ROTRAN]
-
-    fix_atoms = [
-        at
-        for at in st[0].get_atoms()
-        if at.id in SUP_ATOMS
-    ]
-    all_atoms = list(st[0].get_atoms())
-
-    spimp = Superimposer()
-
-    for mod in st:
-        if mod.id == 0:
-            continue
-        mov_atoms = [
-            at
-            for at in st[mod.id].get_atoms()
-            if at.id in SUP_ATOMS
-        ]
-        all_mov_atoms = list(st[mod.id].get_atoms())
-        spimp.set_atoms(fix_atoms, mov_atoms)
-        spimp.apply(st[mod.id].get_atoms())
-        rmsd.append(mu.calc_RMSd_ats(fix_atoms, mov_atoms))
-        rmsd_all.append(mu.calc_RMSd_ats(all_atoms, all_mov_atoms))
-        rotran.append(spimp.rotran)
-    return rotran, rmsd, rmsd_all
 
 def process_hbonds_data(st, hbonds_data):
     ''' Process the X3DNA hydrogen bonds data '''
@@ -127,8 +37,8 @@ def process_hbonds_data(st, hbonds_data):
 
         if hb['donAcc_type'] not in HB_QUALITY:
             continue
-        at1 = get_atom_from_x3dna_id(st, hb['atom1_id'])
-        at2 = get_atom_from_x3dna_id(st, hb['atom2_id'])
+        at1 = ut.get_atom_from_x3dna_id(st, hb['atom1_id'])
+        at2 = ut.get_atom_from_x3dna_id(st, hb['atom2_id'])
         res1 = at1.get_parent()
         res2 = at2.get_parent()
         if res1 == res2:
@@ -154,6 +64,7 @@ def process_hbonds_data(st, hbonds_data):
             'details': hb
         })
     return hb_data
+
 
 class ResidueGroup():
     ''' Class to hold a group of residues '''
@@ -186,7 +97,7 @@ class ResidueGroup():
 
     def superimpose(self):
         ''' Superimpose the models in the group '''
-        self.rotran, self.rmsd_sup, self.rmsd_all = superimpose_models(self.structure)
+        self.rotran, self.rmsd_sup, self.rmsd_all = ut.superimpose_models(self.structure)
 
     def transform_hbcoords(self):
         ''' Transform the hydrogen bond coordinates using groups's rotation and translation
@@ -213,14 +124,17 @@ class ResidueGroup():
         io.set_structure(self.structure)
         io.save(output_file_name)
 
+
 def prepare_groups(st, hb_data):
     ''' Prepare groups of residues to accumulate HB data '''
     groups = {}
     nresidues = 0
     for res in st.get_residues():
-        if res not in hb_data or not hb_data[res] or res.get_resname() not in STD_NUCLEOTIDES:
+        if res not in hb_data or not hb_data[res] or res.get_resname() not in ut.STD_NUCLEOTIDES:
             continue
-
+        # Calc omega angle for the residue
+        omega_dh = ut.calc_omega(res)
+        print(f"#DEBUG OMEGA {mu.residue_id(res)} {omega_dh:3f}")
         # Create a new group if it does not exist
         if res.get_resname() not in groups:
             groups[res.get_resname()] = ResidueGroup(res.get_resname())
@@ -234,11 +148,13 @@ def prepare_groups(st, hb_data):
     print(f"Found {len(hb_data)} hydrogen bonds on {nresidues} residues")
     return groups
 
+
 def apply_transformation(crds, rotran):
     ''' Apply the rotation and translation to the coordinates '''
     rot, tran = rotran
     transformed_crds = np.dot(crds, rot) + tran
     return transformed_crds
+
 
 def process_structure(st, hbonds_data, output_folder, verbose=False):
     ''' Process the structure and X3DNA data to create a PDB file with fake WAT molecules'''
@@ -273,13 +189,13 @@ def main():
     parser = argparse.ArgumentParser(description='Combine X3DNA analysis on a PDB file')
 
     parser.add_argument(
-        '-i','--input_folder',
+        '-i', '--input_folder',
         type=str,
         help='Input Folder (Default current folder)',
         default='.'
     )
     parser.add_argument(
-        '-o','--output_folder',
+        '-o', '--output_folder',
         type=str,
         help='Output Folder (Default input folder)'
     )
@@ -310,7 +226,7 @@ def main():
     # Parse the PDB file
     parser = MMCIFParser()
     structure = parser.get_structure(args.pdb_id, cif_file)
-    atom_renumbering(structure)
+    ut.atom_renumbering(structure)
     print(f"Parsed {args.pdb_id} structure from {cif_file}")
 
     # Load the JSON data
